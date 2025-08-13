52d13679eac6b2471a1363d96dc16bdc8e76eaa6
Ubaydulloh13 <samushakirov@gmail.com>
2025-08-13 11:20:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// // son=prompt("Yoshingizni kiriting kiriting");
+// // if(son>=8 && son<=17){
+// //     alert("Siz mars it schoolga kirshingiz mumkin")
+// // }
+// // else{
+// //     alert("Sizga hali mumkin emas")
+// // }
+// let mashina={
+//     nomi:"Bugutti 100 turbo",
+//     rangi :"ko'k",
+//     yili: 2025,
+//     tanirovka:true,
+//     companiy:"Chevrolet",
+//     nomer:"01 o777oo UZB",
+//     probeg:0,
+//     narxi:"6mln $",
+//     egasi:{
+//         ism:"Ubaydulloh",
+//         familya:"Alimjonv",
+//         Yosh:12,
+//     }
+// }
+// console.log(mashina)
+const odam={
+    name:null,
+    id:null,
+    pasword:null,
+}
+odam.name=prompt("ismkiriting");
+odam.id=prompt("space id kiriting");
+odam.pasword=prompt("Parol kiriting");
+console.log(odam)
